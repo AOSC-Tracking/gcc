46738cc878aaad68e3012566044f2c4da560fec7
Xi Ruoyao <xry111@xry111.site>
2026-05-26 11:01:56 +0800

    DEBIAN: Set gettext's domain and textdomain to the versioned package name
    Link: https://salsa.debian.org/toolchain-team/gcc/-/raw/91b94c546db41a972d37c9626f34a3d655540f74/debian/patches/gcc-textdomain.diff
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>

--- gcc/intl.cc
+++ gcc/intl.cc
@@ -55,8 +55,8 @@ gcc_init_libintl (void)
   setlocale (LC_ALL, "");
 #endif
 
-  (void) bindtextdomain ("gcc", LOCALEDIR);
-  (void) textdomain ("gcc");
+  (void) bindtextdomain ("gcc-16", LOCALEDIR);
+  (void) textdomain ("gcc-16");
 
   /* Opening quotation mark.  */
   open_quote = _("`");
